bd32055fb21d91166cfaaaf8862e4728eae9565b
SUNG IL LEE <sungil.lee90@gmail.com>
2020-08-31 23:56:52 -0500

    init setup for using gradle pack

---
+++ src/main/java/sungilhello/sungilhello/SungilHelloApplication.java
@@ -0,0 +1,13 @@
+package sungilhello.sungilhello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SungilHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SungilHelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sungilhello/sungilhello/SungilHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package sungilhello.sungilhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SungilHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
