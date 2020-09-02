d6868777ccd23ad00a03fe9fe4c8e30483a8c9a0
SUNG IL LEE <sungil.lee90@gmail.com>
2020-09-02 09:57:23 -0500

    added first member test

--- src/test/java/sungilhello/sungilhello/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/sungilhello/sungilhello/repository/MemoryMemberRepositoryTest.java
@@ -1,4 +1,27 @@
 package sungilhello.sungilhello.repository;
+// No need to be "public" bc we are not using it from other classes/packages
+// public class MemoryMemberRepositoryTest {
+// }
 
-public class MemoryMemberRepositoryTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import sungilhello.sungilhello.domain.Member;
+
+class MemoryMemberRepositoryTest {
+
+    MemberRepository repo = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("Spring");
+        
+        repo.save(member);
+        
+        Member result = repo.findById(member.getId()).get();
+        Assertions.assertEquals(member, result);
+        // make sure both member id and result id are the same
+        System.out.println("(result == member) = " + (result == member));
+    }
+
 }
