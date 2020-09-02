99f5ba8636e6db2e423b1ad600775969de334a32
SUNG IL LEE <sungil.lee90@gmail.com>
2020-09-02 10:13:39 -0500

    added findByName test to the system

--- src/test/java/sungilhello/sungilhello/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/sungilhello/sungilhello/repository/MemoryMemberRepositoryTest.java
@@ -7,6 +7,10 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import sungilhello.sungilhello.domain.Member;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class MemoryMemberRepositoryTest {
 
     MemberRepository repo = new MemoryMemberRepository();
@@ -20,9 +24,24 @@ class MemoryMemberRepositoryTest {
         
         Member result = repo.findById(member.getId()).get();
 //        Assertions.assertEquals(member, result);
-        Assertions.assertThat(member).isEqualTo(result);
+        assertThat(member).isEqualTo(result);
         // make sure both member id and result id are the same
         System.out.println("(result == member) = " + (result == member));
     }
 
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("Spring1");
+        repo.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("Spring2");
+        repo.save(member2);
+
+        Optional<Member> result = repo.findByName("Spring1");
+
+        assertThat(result).isEqualTo(result);
+
+    }
 }
