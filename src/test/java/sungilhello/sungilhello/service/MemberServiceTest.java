ad048440c48d16cddb9f43f021186fb9768cbb74
SUNG IL LEE <sungil.lee90@gmail.com>
2020-09-02 15:36:20 -0500

    finished join test

--- src/test/java/sungilhello/sungilhello/service/MemberServiceTest.java
+++ src/test/java/sungilhello/sungilhello/service/MemberServiceTest.java
@@ -1,13 +1,29 @@
 package sungilhello.sungilhello.service;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import sungilhello.sungilhello.domain.Member;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
+    MemberService memberService = new MemberService();
+
+
     @Test
     void join() {
+        // given
+        Member m1 = new Member();
+        m1.setName("hello");
+        // when
+        Long savedId = memberService.join(m1);
+        // then
+        Member findOneMember = memberService.findOne(savedId).get();
+        assertThat(m1.getName()).isEqualTo(findOneMember.getName());
     }
 
     @Test
