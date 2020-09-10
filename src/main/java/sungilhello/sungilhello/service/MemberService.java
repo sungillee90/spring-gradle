a99b43e8f7ab03088051efe7babaf97572ece17a
SUNG IL LEE <sungil.lee90@gmail.com>
2020-09-09 20:56:42 -0500

    finished registerating by using annotaion

--- src/main/java/sungilhello/sungilhello/service/MemberService.java
+++ src/main/java/sungilhello/sungilhello/service/MemberService.java
@@ -1,6 +1,7 @@
 package sungilhello.sungilhello.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import sungilhello.sungilhello.domain.Member;
 import sungilhello.sungilhello.repository.MemberRepository;
@@ -10,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Component
 public class MemberService {
 // cmd + shift + t == Create Test
     private final MemberRepository memberRepository;
