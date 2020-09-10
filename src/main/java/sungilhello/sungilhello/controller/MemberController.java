15b0dcc90fc202888d865e1e606ca8c716c6785e
SUNG IL LEE <sungil.lee90@gmail.com>
2020-09-10 08:41:47 -0500

    init setup for registeration page

--- src/main/java/sungilhello/sungilhello/controller/MemberController.java
+++ src/main/java/sungilhello/sungilhello/controller/MemberController.java
@@ -2,6 +2,7 @@ package sungilhello.sungilhello.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import sungilhello.sungilhello.service.MemberService;
 
 @Controller
@@ -12,6 +13,11 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
     }
 
 }
