dcd717ec4406b13d040281ecbce6954c2f9b49ac
SUNG IL LEE <sungil.lee90@gmail.com>
2020-09-10 15:10:05 -0500

    init setup for view all members page

--- src/main/java/sungilhello/sungilhello/controller/MemberController.java
+++ src/main/java/sungilhello/sungilhello/controller/MemberController.java
@@ -2,10 +2,13 @@ package sungilhello.sungilhello.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import sungilhello.sungilhello.domain.Member;
 import sungilhello.sungilhello.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -32,6 +35,14 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String viewAllMembers(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+
+        return "members/viewAllMembers";
     }
 
 }
