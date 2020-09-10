5996a11ef8ab5de1df4b2f8af553f7044f965b79
SUNG IL LEE <sungil.lee90@gmail.com>
2020-09-10 09:00:49 -0500

    Finished Register MVC

--- src/main/java/sungilhello/sungilhello/controller/MemberController.java
+++ src/main/java/sungilhello/sungilhello/controller/MemberController.java
@@ -3,6 +3,8 @@ package sungilhello.sungilhello.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import sungilhello.sungilhello.domain.Member;
 import sungilhello.sungilhello.service.MemberService;
 
 @Controller
@@ -18,6 +20,18 @@ public class MemberController {
     @GetMapping("/members/new")
     public String createForm() {
         return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+//        System.out.println("User Entered Name = " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 
 }
